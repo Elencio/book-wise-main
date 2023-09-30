8db8dd83b978601e39dfdddef25d603f3af4d90c
elenciozivane <elenciocalado@gmail.com>
2023-09-30 00:39:23 -0300

    SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -31,6 +32,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.bookwise.com',
+            siteName: 'Book Wise',
+          }}
+        />
         <div className={nunito.className}>
           {getLayout(<Component {...pageProps} />)}
         </div>
